a2e22a2ffc6302007b94930c39e45c143ae57d7f
brunamy <bm6952@gmail.com>
2023-06-23 15:15:10 -0300

    aula13

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "corsheaders",
     "rest_framework",
+    "rest_framework_simplejwt",
     "livraria",
 ]
 
@@ -115,12 +116,12 @@ USE_I18N = True
 USE_TZ = True
 
 REST_FRAMEWORK = {
-#     "DEFAULT_PERMISSION_CLASSES": [
-#         "rest_framework.permissions.IsAuthenticated",
-#     ]  
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissions",
     ],
+     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
     
 
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from rest_framework.routers import DefaultRouter
 
@@ -13,6 +17,8 @@ router.register(r"editoras", EditoraViewSet)
 router.register(r"livros", LivroViewSet)
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"), 
     path('admin/', admin.site.urls),
     path("", include(router.urls)),
 ]
