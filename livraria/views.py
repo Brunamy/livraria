57b98c0fee8fd9e832318c021ac84a4943d63495
brunamy <bm6952@gmail.com>
2023-06-23 14:22:30 -0300

    aula12/permissao

--- livraria/views.py
+++ livraria/views.py
@@ -1,20 +1,29 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria
-from livraria.serializers import CategoriaSerializer
+from livraria.models import Autor, Categoria, Editora, Livro
+from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroListSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-
-class LivroViewSet(ModelViewSet):
-    queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
+    permission_classes = [IsAuthenticated]
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
 
-class AutorViewSet(ModelViewSet):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializer
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    # serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return LivroListSerializer
+        elif self.action == "retrieve":
+            return LivroDetailSerializer
+        return LivroSerializer
